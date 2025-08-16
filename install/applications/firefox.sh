37de34862759da8f3c688fc6ca394119dbfb369a
Bjørn Kristian Strand <bjkristian@proton.me>
2025-08-16 14:44:30 +0200

    .

--- install/applications/firefox.sh
+++ install/applications/firefox.sh
@@ -20,4 +20,4 @@ Pin: origin packages.mozilla.org
 Pin-Priority: 1000
 ' | sudo tee /etc/apt/preferences.d/mozilla
 
-sudo apt update && sudo apt install firefox -y
+sudo apt update && sudo apt install firefox -y --allow-downgrades
