a5cfd6c9fef52a605f62ba2ad9c89998a146257c
Bjørn Kristian Strand <bjkristian@proton.me>
2025-08-23 15:03:38 +0200

    .

--- boot.sh
+++ boot.sh
@@ -14,7 +14,7 @@ ___.    __           .__          __  .__
 echo -e "$ascii_art"
 
 sudo apt update > /dev/null
-sudo apt install git > /dev/null
+sudo apt install git curl > /dev/null
 
 echo "Cloning ubuntu install..."
 rm -fr ~/.local/share/ubuntu-install
